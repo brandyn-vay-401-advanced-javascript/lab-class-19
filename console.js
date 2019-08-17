caa690141d18def720022e7a951ba1efb43a5b8b
Brandyn Vay <brandynvay@protonmail.com>
2019-08-17 13:12:28 -0700

    starting

--- console.js
+++ console.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const theword = require('theword.js');
+const theword = require('./theword.js');
 
 const io = require('socket.io-client');
 
-const socket = io.connect('http://172.16.0.231:3000');
+const socket = io.connect('http://localhost:3000');
 
 socket.on('the-bird', theword);
 
